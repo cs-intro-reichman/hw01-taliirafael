271b8f0f6c0b1e92b909c577bcbefa6b52c9c7f7
taliirafael <154661297+taliirafael@users.noreply.github.com>
2023-12-25 01:13:38 +1100

    Update GenThree.java
    code attempt #1

--- GenThree.java
+++ GenThree.java
@@ -3,7 +3,15 @@
  * prints them, and then prints the minimal number that was generated.
  */
 public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+    public static void main(String[] args) {
+    int a = Integer.parseInt(args[0]); int b = Integer.parseInt(args[1]);
+    int m = a + (int)(Math.random() * (b - a)); 
+    int n = a + (int)(Math.random() * (b - a)); 
+    int o = a + (int)(Math.random() * (b - a));
+        int min = Math.min(m, Math.min(n, o)); 
+    System.out.println(m);
+    System.out.println(n);
+    System.out.println(o);
+        System.out.println("The minimal generated number was " + min); 
+    }
 }
