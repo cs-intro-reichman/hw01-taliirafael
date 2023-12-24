64572aa04fad0553e464328f9cbb79e8cad95444
taliirafael <154661297+taliirafael@users.noreply.github.com>
2023-12-25 01:03:40 +1100

    Update Triangle.java
    code attempt 1

--- Triangle.java
+++ Triangle.java
@@ -4,7 +4,11 @@
  * Write a program that tests if three given integers form a triangle.
  */ 
 public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+    public static void main(String[] args){
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    int c = Integer.parseInt(args[2]);
+    boolean canFormTriangle = (a + b > c) && (a + c > b) && (b + c > a); 
+    System.out.println(a + ", " + b + ", " + c + ": " + canFormTriangle);
+    } 
 }
